21612235acdd24b390022974fa61e6bd205ede71
robaya <robaya05@gmail.com>
2026-06-04 09:08:52 +0700

    Rilis Pertama: Bot WA PPDB Interaktif Baileys

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     // Pastikan Laravel Anda berjalan (php artisan serve) biasanya di port 8000
-    //API_URL: 'https://ppdb.ponpesassaadah.com/api/ppdb',
-    API_URL: 'http://127.0.0.1:8000/api/ppdb',
+    API_URL: 'https://ppdb.ponpesassaadah.com/api/ppdb',
+    //API_URL: 'http://127.0.0.1:8000/api/ppdb',
 
     // Tambahkan daftar nomor admin PPDB di sini
     ADMIN_PPDB: ['6285136468097', '6285797750256', '6285188427706'],
